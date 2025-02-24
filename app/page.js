d3a06646126471ef9d17c53134c0a3b82005b9f8
hectorlabra <121694325+hectorlabra@users.noreply.github.com>
2025-02-23 22:32:09 -0300

    updated the hero section

--- app/page.js
+++ app/page.js
@@ -3,7 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <button className="btn">Start Now</button>
+      <h1>Collect customer feedback to build better products</h1>
+      <div>
+        Create a feedback board in minutes, prioritaze features, and build
+        products your customer will love.
+      </div>
     </main>
   );
 }
