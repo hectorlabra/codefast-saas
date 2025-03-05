edf9c9eaeaf270bd375b612f7cb0afb27d0182f8
hectorlabra <121694325+hectorlabra@users.noreply.github.com>
2025-03-05 13:32:13 -0300

    add pricing and faq section

--- components/ButtonLogin.js
+++ components/ButtonLogin.js
@@ -1,12 +1,11 @@
 import Link from "next/link";
 
-const ButtonLogin = ({isLoggedIn, name}) => {
+const ButtonLogin = ({isLoggedIn, name, extraStyle}) => {
   if (isLoggedIn) {
-    return <Link href="/dashboard" className="btn btn-primary">you're logged in! {name}</Link>;
+    return <Link href="/dashboard" className={`btn btn-primary ${extraStyle ? extraStyle : ""}`}>you&apos;re logged in! {name}</Link>;
   } else {
     return <button>Login</button>
   }
 };
-
 
 export default ButtonLogin;
