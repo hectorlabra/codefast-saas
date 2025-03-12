3796dba2826935a67be6a38d601d17cb60e927e0
hectorlabra <121694325+hectorlabra@users.noreply.github.com>
2025-03-12 19:22:21 -0300

    add Resend and Google providers to NextAuth configuration

--- auth.js
+++ auth.js
@@ -1,10 +1,23 @@
 import NextAuth from "next-auth";
+import Resend from "next-auth/providers/resend";
+import Google from "next-auth/providers/google";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import clientPromise from "./libs/mongo";
 
 const config = {
-    providers: [],
-    adapter: MongoDBAdapter(clientPromise),
+  providers: [
+    Resend({
+      apiKey: process.env.RESEND_KEY,
+      from: "noreply@codefastsaas.hectorlabra.dev",
+      name: "Email",
+    }),
+    Google({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      allowDangerousEmailAccountLinking: true,
+    }),
+  ],
+  adapter: MongoDBAdapter(clientPromise),
 };
 
 export const { handlers, signIn, signOut, auth } = NextAuth(config);
